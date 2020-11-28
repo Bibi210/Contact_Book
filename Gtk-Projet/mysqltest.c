2c1dec85f528b6f3b99ea1192b2c5ab30905ca32
Bibi210 <brahimadibassi@gmail.com>
2020-11-28 04:10:36 +0100

    Road to Master SQLITE part 1

--- Gtk-Projet/mysqltest.c
+++ Gtk-Projet/mysqltest.c
@@ -2,50 +2,100 @@
 #include <gtk/gtk.h>
 #define CONTACT_SIZE ((guint8)9)
 
-typedef GArray Contact_t;
+sqlite3 *_db;
 
-void data_base_init(sqlite3 *db)
+void data_base_init()
 {
     gchar *errorsql = "Erreur sur la base de données";
-    if (sqlite3_open("contacts_base.db", &db) != SQLITE_OK)
+    if (sqlite3_open("contacts_base.db", &_db) != SQLITE_OK)
     {
         g_print("Impossible d'ouvrir la base de contacts");
         exit(-1);
     }
 
-    if (sqlite3_exec(db, "SELECT * FROM contact_list;", 0, 0, &errorsql) != SQLITE_OK)
+    if (sqlite3_exec(_db, "SELECT * FROM contact_list;", 0, 0, &errorsql) != SQLITE_OK)
     {
         g_print("Creation de la table \n");
-        sqlite3_exec(db, "CREATE TABLE contact_list (id INTEGER PRIMARY KEY AUTOINCREMENT,nom TEXT,prenom,TEXT,email TEXT,Adresse: TEXT,type TEXT,num1 TEXT,num2 TEXT,num3 TEXT,notes TEXT);", 0, 0, &errorsql);
+        sqlite3_exec(_db, "CREATE TABLE contact_list (id TEXT,nom TEXT,prenom,TEXT,email TEXT,adresse TEXT,type TEXT,num1 TEXT,num2 TEXT,num3 TEXT,notes TEXT);", 0, 0, &errorsql);
         //* Exemple Pour insert value sqlite3_exec(db,"INSERT INTO contact_list (nom,prenom) VALUES('Brahima','Dibassi');",0,0,&errorsql);
     }
-    sqlite3_exec(db, ".backup main  contacts_base.db;", 0, 0, &errorsql);
-    sqlite3_close(db);
-}   
-
-void data_base_add_contact(Contact_t *un_type)
-{
-    //TODO sqlite3_bind_text(phrase en preparation ,Position du ?,Value a bind,Taille '-1',Callback);
+    sqlite3_exec(_db, ".backup main  contacts_base.db;", 0, 0, &errorsql);
 }
 
+void data_base_add_contact(GList *un_type)
+{
+    gint i = 1;
+    sqlite3_stmt *commande;
+    sqlite3_prepare_v2(_db, "INSERT INTO contact_list (id,nom,prenom,email,adresse,type,num1,num2,num3,notes) VALUES(?,?,?,?,?,?,?,?,?,?);", -1, &commande, NULL);
+
+    for (GList *encours = un_type; encours != NULL; encours = encours->next)
+    {
+        sqlite3_bind_text(commande, i, encours->data, -1, NULL);
+        i++;
+    }
+    sqlite3_step(commande);
+    sqlite3_finalize(commande);
+}
+
+void data_base_del_contact(gchar* id){
+    sqlite3_stmt *commande;
+    sqlite3_prepare_v2(_db, "DELETE FROM contact_list WHERE id = ?;", -1, &commande, NULL);
+    sqlite3_bind_text(commande,1,id,-1,NULL);
+    sqlite3_step(commande);
+    sqlite3_finalize(commande);
+}
+
+void data_base_modify_contact(GList *contact_data){
+    data_base_del_contact(contact_data->data);
+    data_base_add_contact(contact_data);
+}
+
+int print_data_callback(void *useless, int argc, char **argv,char **azColName){
+    
+    for (gint i=0;i < argc;i++){
+        g_print("%s = %s\n",azColName[i],argv[i]? argv[i]:"...."); 
+    }
+    (void) useless;
+    return 0;
+}
+void data_base_print(){
+    sqlite3_exec(_db,"SELECT * FROM contact_list ORDER BY nom",print_data_callback,0,NULL);
+}
+
+void data_base_lookup(gchar* recherche){
+    gchar* com = "SELECT * FROM contact_list WHERE nom ='";
+    gchar* full_com = g_strjoin("",com,recherche,"';",NULL);
+    sqlite3_exec(_db, full_com,print_data_callback,0,NULL);
+}
 
 int main()
 {
-    sqlite3 *db;
-    data_base_init(db);
+    data_base_init();
 
-    gchar *test[CONTACT_SIZE];
-    test[0] = "Dibassi";
-    test[1] = "Brahima";
-    test[2] = "brahimadibassi@gmail.com";
-    test[3] = "20 rue sabran de ponteves";
-    test[4] = "Pro";
-    test[5] = "0615261588";
-    test[6] = "";
-    test[7] = "";
-    test[8] = "Trop beau ce boug";
+    GList *test_contact = NULL;
+    test_contact = g_list_append(test_contact, "1");
+    test_contact = g_list_append(test_contact, "Dibassi");
+    test_contact = g_list_append(test_contact, "Brahima");
+    test_contact = g_list_append(test_contact, "brahimadibassi@gmail.com");
+    test_contact = g_list_append(test_contact, "20 rue sabran de ponteves");
+    test_contact = g_list_append(test_contact, "Pro");
+    test_contact = g_list_append(test_contact, "0615261588");
+    test_contact = g_list_append(test_contact, "");
+    test_contact = g_list_append(test_contact, "");
+    test_contact = g_list_append(test_contact, "Trop beau ce boug");
 
-    g_print("%s \n", test[8]);
+    GList *bibi = NULL;
+    bibi = g_list_append(bibi, "6");
+    bibi = g_list_append(bibi, "Zidane");
+    bibi = g_list_append(bibi, "Dibassi");
+    data_base_add_contact(bibi);
+    data_base_add_contact(test_contact);
+    
+    data_base_lookup("Dibassi");
 
-
+    //! gcc -fanalyzer -Wall -Wextra mysqltest.c -o  mysqltest -g `pkg-config --cflags --libs gtk+-3.0` -lsqlite3  && mysqltest
+    //* qlite3_bind_text(phrase en preparation ,Position du ?,Value a bind,Taille '-1',Callback);
+    //* select * from contact_list WHERE id = '2'
+    data_base_del_contact("1");
+    sqlite3_close(_db);
 }
